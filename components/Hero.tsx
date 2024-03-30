c7843a2fbff6e7ba7e8a1b1dc1ce6f3cac4fcb4c
devdalgic <denizhandalgic@outlook.com>
2024-03-30 23:54:14 +0300

    Removed RoughNotation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {RoughNotation, RoughNotationGroup} from "react-rough-notation";
 
 export default function Hero() {
     const animationDuration = Math.floor(200);
@@ -10,64 +9,18 @@ export default function Hero() {
     return (
         <div className="flex flex-col lg:flex-row items-center content-center pb-4 px-36">
             <div className="text-center lg:text-start lg:me-20">
-                <RoughNotationGroup show={true}>
-                    <RoughNotation
-                        type="underline"
-                        multiline={true}
-                        padding={[0, 2]}
-                        iterations={iterations}
-                        animationDelay={animationDelay}
-                        animationDuration={animationDuration}
-                        color={colors[3]}
-                        strokeWidth={strokeWidth}
-                        >
-                        <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
-                            Engineer.
-                        </h1>
-                    </RoughNotation>
-                    <RoughNotation
-                        type="underline"
-                        multiline={true}
-                        padding={[0, 2]}
-                        iterations={iterations}
-                        animationDelay={animationDelay}
-                        animationDuration={animationDuration}
-                        color={colors[3]}
-                        strokeWidth={strokeWidth}
-                    >
-                        <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
-                            Developer.
-                        </h1>
-                    </RoughNotation>
-                    <RoughNotation
-                        type="underline"
-                        multiline={true}
-                        padding={[0, 5]}
-                        iterations={iterations}
-                        animationDelay={animationDelay}
-                        animationDuration={animationDuration}
-                        color={colors[3]}
-                        strokeWidth={strokeWidth}
-                    >
-                        <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
-                            Blogger.
-                        </h1>
-                    </RoughNotation>
-                    <RoughNotation
-                        type="underline"
-                        multiline={true}
-                        padding={[0, 5]}
-                        iterations={iterations}
-                        animationDelay={animationDelay}
-                        animationDuration={animationDuration}
-                        color={colors[3]}
-                        strokeWidth={strokeWidth}
-                    >
-                        <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
-                            Youtuber.
-                        </h1>
-                    </RoughNotation>
-                </RoughNotationGroup>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
+                    Engineer.
+                </h1>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
+                    Developer.
+                </h1>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
+                    Blogger.
+                </h1>
+                <h1 className="text-4xl md:text-8xl font-bold text-gray-200 my-2">
+                    Youtuber.
+                </h1>
             </div>
             {/* Image container */}
             <div className="mt-4 g:mt-20">
