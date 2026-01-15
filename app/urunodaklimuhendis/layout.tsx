d6cc40e76ebfc4fe526033847f6573b2491f5b83
devdalgic <denizhandalgic@outlook.com>
2026-01-15 14:45:16 +0300

    Added new page

---
+++ app/urunodaklimuhendis/layout.tsx
@@ -0,0 +1,40 @@
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import '../globals.css'
+import React from "react";
+import {AptabaseProvider} from "@aptabase/react";
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Denizhan Dalgic Portfolio Website',
+    description: 'Personal website of Denizhan Dalgic',
+    keywords: ["denizhan", "dalgic", "denizhandalgic", "denizhan dalgic", "denizhan dalgıç", "portfolio", "devdalgic"],
+    creator: "Denizhan Dalgic",
+    authors: [{name: "Denizhan Dalgic"}],
+    robots: "index, follow",
+    openGraph: {
+        type: "website",
+        url: "https://devdalgic.github.io",
+        title: "Denizhan Dalgic Portfolio Website",
+        description: "Personal website of Denizhan Dalgic",
+        siteName: "Denizhan Dalgic Portfolio Website",
+        images: [{
+            url: "/dd.webp",
+        }],
+    }
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+      <AptabaseProvider appKey="A-US-9847557599">{children}</AptabaseProvider>
+      </body>
+    </html>
+  )
+}
